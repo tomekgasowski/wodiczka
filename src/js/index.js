26896607a6a2728b17a5ed84e29fb84a823ea32f
Tomek Gasowski <tomekgasowski93@gmail.com>
2022-02-02 20:50:29 +0100

    js and html improvment

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,34 @@ import '../scss/main.scss';
 
 /* place your code below */
 
+let counter = document.querySelector('.number--js');
+let moreWater = document.querySelector('.increaseButton--js');
+let lessWater = document.querySelector('.decreaseButton--js');
+
+const key = new Date().toISOString().slice(0,10);
+
+if (!(localStorage.getItem(key))) {
+    localStorage.setItem(key, 0);
+    counter.innerHTML = 0;
+}
+else {
+    counter.innerHTML = localStorage.getItem(key);
+}
+
+let numberOfGlass = localStorage.getItem(key);
+
+moreWater.addEventListener('click', () => {
+    if (numberOfGlass < 99) {
+    numberOfGlass = Number(numberOfGlass) + 1;
+    counter.innerHTML = numberOfGlass;
+    localStorage.setItem(key, numberOfGlass);
+    }
+});
+
+lessWater.addEventListener('click', () => {
+    if (numberOfGlass > 0) {
+        numberOfGlass = Number(numberOfGlass) - 1;
+        counter.innerHTML = numberOfGlass;
+        localStorage.setItem(key, numberOfGlass);
+    }
+});
